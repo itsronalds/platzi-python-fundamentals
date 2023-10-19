ae375422577c96b780e9a3d8128163475974dd92
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-10-19 18:44:11 -0400

    Numbers lesson

---
+++ src/05_numbers.py
@@ -0,0 +1,36 @@
+# Integers
+lives = 3
+print(type(lives))
+
+age = 25
+budget = 1_000_000
+
+# Floats
+temperature = 12.12
+print(type(temperature))
+
+# Re-assigning variables
+lives = 2
+print(lives)
+
+# Addition
+lives = 12 + 15
+print(lives)
+
+# Subtraction
+lives = lives - 1
+print(lives)
+
+# Pythonic way to arithmetics operations
+lives += 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+# Large numbers (scientific notation)
+large_number = 4500000000000000000.1
+print(large_number)
+
+large_number_b = 0.0000000000000001
+print(large_number_b)
