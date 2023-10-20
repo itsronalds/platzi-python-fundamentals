9a6d46b9e5c760b9b77e4aa60fc8619fd3ac07dc
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-10-20 12:23:27 -0400

    Booleans lesson

---
+++ src/06_booleans.py
@@ -0,0 +1,18 @@
+'''
+Booleans are a data type that can only be True or False.
+
+In bynary, True is 1 and False is 0.
+'''
+
+is_single = True
+print(type(is_single))
+
+is_single = False
+print(is_single)
+
+# Using the not (negation) operator
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
