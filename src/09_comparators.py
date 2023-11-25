192c6e6f85a1292fd7ad463c65be59cad65b36c5
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-11-25 17:03:55 -0400

    Added comparators lesson

---
+++ src/09_comparators.py
@@ -0,0 +1,83 @@
+# Greater than >
+
+# ✅ True
+print(7 > 3)
+
+# ❌ False
+print(3 > 7)
+
+# ❌ False
+print(7 > 7)
+
+# Less than <
+
+# ✅ True
+print(5 < 6)
+
+# ❌ False
+print(6 < 5)
+
+# ❌ False
+print(5 < 5)
+
+# Greater than or equal to >=
+
+# ✅ True
+print(2 >= 1)
+
+# ❌ False
+print(2 >= 3)
+
+# ✅ True
+print(2 >= 2)
+
+# Less than or equal to <=
+
+# ✅ True
+print(1 <= 2)
+
+# ❌ False
+print(2 <= 1)
+
+# ✅ True
+print(2 <= 2)
+
+# Equal to ==
+
+# ✅ True
+print(6 == 6)
+
+# ❌ False
+print(5 == 2)
+
+# Not equal to !=
+
+# ✅ True
+print(6 != 10)
+
+# ❌ False
+print(6 != 6)
+
+'''
+String comparison
+'''
+
+# ✅ True
+print('Apple' == 'Apple')
+
+# ❌ False
+print('Apple' == 'apple')
+
+# ❌ False
+print('1' == 1)
+
+# Check the age
+age = 18
+
+# ✅ True
+print(age >= 18)
+
+age = 15
+
+# ❌ False
+print(age >= 18)
