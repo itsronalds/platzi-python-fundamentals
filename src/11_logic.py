cb1e0554cd1eb0e17b88dec032da4a23a8d412b8
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-12-06 16:03:27 -0400

    Added logic operators lesson

---
+++ src/11_logic.py
@@ -0,0 +1,29 @@
+# and
+print('AND')
+
+print('True and True =>', True and True)
+print('True and False =>', True and False)
+print('False and True =>', False and True)
+print('False and False =>', False and False)
+
+# Using numbers
+print(10 > 5 and 5 < 10)
+print(10 > 5 and 5 > 10)
+
+stock = input('Enter the stock number => ')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
+
+# or
+
+print('OR')
+
+print('True or True =>', True or True)
+print('True or False =>', True or False)
+print('False or True =>', False or True)
+print('False or False =>', False or False)
+
+role = input('Enter the role => ')
+
+print(role == 'admin' or role == 'seller')
